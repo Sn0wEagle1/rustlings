a68792a39032c383c159d28909f0df6a8c368689
Vyacheslav <dudarev.slavik@mail.ru>
2023-07-04 02:05:57 +0300

    Add files via upload

--- exercises/move_semantics/move_semantics6.rs
+++ exercises/move_semantics/move_semantics6.rs
@@ -1,28 +1,28 @@
 // move_semantics6.rs
-//
+// Execute `rustlings hint move_semantics6` or use the `hint` watch subcommand for a hint.
 // You can't change anything except adding or removing references.
-//
-// Execute `rustlings hint move_semantics6` or use the `hint` watch subcommand
-// for a hint.
 
-// I AM NOT DONE
 
 fn main() {
     let data = "Rust is great!".to_string();
 
-    get_char(data);
+    let last_char = get_char(&data);
+    println!("Last character: {}", last_char);
 
-    string_uppercase(&data);
+    let uppercased_data = string_uppercase(data);
+    println!("Uppercased data: {}", uppercased_data);
 }
 
 // Should not take ownership
-fn get_char(data: String) -> char {
+fn get_char(data: &str) -> char {
     data.chars().last().unwrap()
 }
 
 // Should take ownership
-fn string_uppercase(mut data: &String) {
-    data = &data.to_uppercase();
+fn string_uppercase(mut data: String) -> String {
+    data = data.to_uppercase();
 
     println!("{}", data);
+
+    data
 }
